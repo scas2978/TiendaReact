14effa87d2db39727ce5bac0814fbfaefc99d579
scas2978 <santi007374234@outlook.com>
2023-03-14 15:29:19 -0500

    servicios

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React,{ Component } from "react";
 import Navegacion from "./componentes/Navegacion/Navegacion";
+import servicios from "./componentes/Servicios/servicios";
 
 function App() {
   return (
     <div className="App">
        <Navegacion></Navegacion>
-    </div>
-  );
+<servicios></servicios>
+       </div>);
+
+  
 }
 
 export default App;
--- src/componentes/Servicios/servicios.js
+++ src/componentes/Servicios/servicios.js
@@ -4,6 +4,30 @@ import './servicios.css';
 class servicios extends Component {
   render() {
     return (
+        <section  id="servicio">
+        <h2 class="subtitulo">Nuestro servicios </h2>
+        <div class="checklist-servicios">
+            <div class="contenedor-servicio servicio">
+                <img src="./Designer _Flatline.svg"  width="100%" alt=""/>
+            </div>
+            <div class="servicio">
+            <div class="service">
+            <h3 class="n-service"> <span class="number">1</span>Diseño web</h3>
+            <p>El diseño web en un area enfocada en el desarrollo de interface digitales, como el diseño de sitios y aplicaciones para web</p>
+            </div>
+
+            <div class="service">
+                <h3 class="n-service"> <span class="number">2</span>Tecnologia</h3>
+                <p>Venta de todo tipo de articulos tecnologicos para mejorar la calidad de vida y agilizar tus tareas diarias</p>
+            </div>
+
+                <div class="service">
+                    <h3 class="n-service"> <span class="number">3</span>Asesoria</h3>
+                    <p>Te compartimos la informacion de los mejores productos en la actualidad con el fin de que siempre estes al tanto de los ultimos avances tecnologicos</p>
+                </div>
+            </div>
+            </div>
+    </section>
 
     ) ;
   }
