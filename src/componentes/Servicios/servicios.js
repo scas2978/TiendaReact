4416f510b89b5264e8e9b28b18e3debe6a9c5ba4
scas2978 <santi007374234@outlook.com>
2023-03-14 15:20:07 -0500

    componente servicios

---
+++ src/componentes/Servicios/servicios.js
@@ -0,0 +1,12 @@
+import React,{ Component} from 'react';
+import './servicios.css';
+
+class servicios extends Component {
+  render() {
+    return (
+
+    ) ;
+  }
+}
+
+export default servicios;
