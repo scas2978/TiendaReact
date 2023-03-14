be23c44abb40fefc39156dcf280741c5d8c74863
scas2978 <santi007374234@outlook.com>
2023-03-14 18:36:44 -0500

    abrir imagenes

--- src/componentes/Portafolio/portafolio.js
+++ src/componentes/Portafolio/portafolio.js
@@ -1,37 +1,34 @@
-import React,{ Component} from 'react';
+import React, { Component } from 'react';
 import './portafolio.css';
-import  imagen from "./Teclado.avif";
-import  imagen2 from "./Ram.avif";
-import  imagen3 from "./Pc.avif";
-import  imagen4 from "./Celular.avif";
-import  imagen5 from "./Xbox.avif";
-import  imagen6 from "./img6.jpg";
-class portafolio extends Component {
+import imagen from "./Teclado.avif";
+import imagen2 from "./Ram.avif";
+import imagen3 from "./Pc.avif";
+import imagen4 from "./Celular.avif";
+import imagen5 from "./Xbox.avif";
+import imagen6 from "./img6.jpg";
+
+class Portafolio extends Component {
+  handleOpenImage = (imageUrl) => {
+    window.open(imageUrl, '_blank');
+  }
+
   render() {
     return (
-        <section class="gallery" id="portafolio">
-        <div class="contenedor-galeria">
-            <h2 class="subtitulo">Galeria</h2>
-            <div class="contenedor-galeria">
-                
-                <img src= {imagen} width="100%" alt="./Teclado.avif" class="img-galeria"/>
-                          
-                <img src= {imagen2} width="100%" alt="./Ram.avif" class="img-galeria"/>
-             
-                <img src= {imagen3} width="100%" alt="./Pc.avif" class="img-galeria"/>
-             
-                <img src= {imagen4} width="100%" alt="./Celular.avif" class="img-galeria"/>
-             
-                <img src= {imagen5} width="100%" alt="./Xbox.avif" class="img-galeria"/>
-
-                <img src= {imagen6} width="100%" alt="./img6.jpg" class="img-galeria"/>
-             
-       
-            </div>
+      <section className="gallery" id="portafolio">
+        <div className="contenedor-galeria">
+          <h2 className="subtitulo">Galeria</h2>
+          <div className="contenedor-galeria">
+            <img src={imagen} width="100%" alt="./Teclado.avif" className="img-galeria" onClick={() => this.handleOpenImage(imagen)} />
+            <img src={imagen2} width="100%" alt="./Ram.avif" className="img-galeria" onClick={() => this.handleOpenImage(imagen2)} />
+            <img src={imagen3} width="100%" alt="./Pc.avif" className="img-galeria" onClick={() => this.handleOpenImage(imagen3)} />
+            <img src={imagen4} width="100%" alt="./Celular.avif" className="img-galeria" onClick={() => this.handleOpenImage(imagen4)} />
+            <img src={imagen5} width="100%" alt="./Xbox.avif" className="img-galeria" onClick={() => this.handleOpenImage(imagen5)} />
+            <img src={imagen6} width="100%" alt="./img6.jpg" className="img-galeria" onClick={() => this.handleOpenImage(imagen6)} />
+          </div>
         </div>
-    </section>
-    ) ;
+      </section>
+    );
   }
 }
 
-export default portafolio;
+export default Portafolio;
