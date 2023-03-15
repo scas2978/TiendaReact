a2c341288ad5eace3e43c28da8da5eae3747458d
santiago525 <santiagose2020@gmail.com>
2023-03-14 19:59:10 -0500

    integracion appi

--- src/componentes/Navegacion/Navegacion.js
+++ src/componentes/Navegacion/Navegacion.js
@@ -1,45 +1,28 @@
-import React, { Component } from 'react';
+import React,{ Component} from 'react';
 import './Navegacion.css';
-import './script';
 import imagen from './hamburguesa.svg';
 
 class Navegacion extends Component {
-  componentDidMount() {
-    const hamburguer = document.querySelector('.hamburguer');
-    const menu = document.querySelector('.menu-navegacion');
 
-    hamburguer.addEventListener('click', () => {
-      menu.classList.toggle('spread');
-    });
-
-    window.addEventListener('click', (e) => {
-      if (
-        menu.classList.contains('spread') &&
-        e.target != menu &&
-        e.target != hamburguer
-      ) {
-        menu.classList.toggle('spread');
-      }
-    });
-  }
-
+  
   render() {
     return (
       <header class="header" id="inicio">
         <img src={imagen} class="hamburguer" />
-        <nav class="menu-navegacion">
+       
+          <nav class="menu-navegacion">
           <a href="#inicio">inicio</a>
           <a href="#servicio">servicio</a>
           <a href="#portafolio">portafolio</a>
           <a href="#expertos">expertos</a>
           <a href="#contacto">contacto</a>
-        </nav>
-        <div class="contenedor head">
-          <h1 class="titulo">convertimos tus sueños en realidad</h1>
-          <p class="copy">Te asesoramos</p>
-        </div>
-      </header>
-    );
+          </nav>
+          <div class="contenedor head">
+              <h1 class="titulo">convertimos tus sueños en realidad</h1>
+              <p class="copy" >Te asesoramos</p>
+          </div>
+        </header>
+    ) ;
   }
 }
 
